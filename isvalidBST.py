6ce557df4d76e6eb1518e9c0d01fbca307c0b39c
Chansa Chishimba <chansazm@yahoo.co.uk>
2020-12-08 07:06:08 -0600

    Added isvalidBST

---
+++ isvalidBST.py
@@ -0,0 +1,15 @@
+class Node(object):
+    def __init__(self,val,left = None, right=None):
+        self.value = val
+        self.left = left
+        self.right = right
+        
+        
+class solution(object):
+    def isValid(self,n):
+        return False
+        
+        
+node = Node(5)
+node.left(4)
+node.right(6)
