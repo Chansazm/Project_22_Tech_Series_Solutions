9291606d75d07e63d3fcf4e2706cf052d686088e
Chansa Chishimba <chansazm@yahoo.co.uk>
2020-12-09 10:50:59 -0600

    Added reconstruct

---
+++ reconstructqueue.py
@@ -0,0 +1,9 @@
+class solution(object):
+    def reconstruct(self,nums):
+        nums.sort(key = lambda x:(-x[0],x[1]))
+        return nums
+
+
+
+nums = [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]
+print(solution().reconstruct(nums))
