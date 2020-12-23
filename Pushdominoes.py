27c6fdbea11ad19df86bce29cbd1ea857994f633
Chansa Chishimba <chansazm@yahoo.co.uk>
2020-12-23 07:09:01 -0600

    Added push dominoes

--- Pushdominoes.py
+++ Pushdominoes.py
@@ -2,7 +2,8 @@ class solution(object):
     def push_dominoes(self,dominoes):
         forces = [0] * len(dominoes)
         max_force = len(dominoes)
-        for i, d in dominoes:
+        force = 0
+        for i, d in enumerate(dominoes):
             if d == 'R':
                 force = max_force
             elif d == 'L':
@@ -10,9 +11,27 @@ class solution(object):
             else:
                 force = max(0,force -1)
             forces[i] = force
+        
+        for i, d in enumerate(dominoes):
+            if d == 'L':
+                force = max_force
+            elif d == 'R':
+                force = 0
+            else:
+                force = max(0,force -1)
+            forces[i] -= force
+            
+        Results = ''
+        for f in forces:
+            if f == 0:
+                Results += '.'
+            elif f > 0:
+                Results += 'R'
+            else:
+                Results += 'L'
+        return Results
+              
                 
-                
-                
-                
+               
 #Driver function
-print(solution.push_dominoes())
+print(solution().push_dominoes('R..L..RR'))
